a7abe17d34744dbbfb4079309795893fec926236
Orlando <83675900+Orlando438@users.noreply.github.com>
2025-05-20 01:53:26 -0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -23,7 +23,7 @@ async function main() {
     messages: [
       {
         role: "user",
-        content: `Bom base na diff informada analise e faça um comentário breve e curto para quem vai testar a tarefa descreverndo o que muda e possíveis impactos:\n\n${diff}`
+        content: `Bom base na diff informada analise e faça um comentário breve e curto para quem vai testar a tarefa descreverndo o que muda e possíveis impactos, lembrando que você está expliando para um teste, alguém sem experiencia nenhuma com desenvolvimento:\n\n${diff}`
       }
     ]
   });
